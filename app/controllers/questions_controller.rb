fa444e8e22e6dfc2457a40608a519d612013c1e4
bickelmann brice <bickelmann.b@gmail.com>
2019-05-20 15:32:02 +0200

    good style correct

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,14 +3,9 @@ class QuestionsController < ApplicationController
   end
 
   def answer
-    @ask = params[:ask]
-    @answer = ""
-    if @ask == 'I am going to work'
-      @answer = 'Great!'
-    elsif @ask.end_with?("?")
-      @answer = 'Silly question, get dressed and go to work'
-    else
-      @answer = "I don't care, get dressed and go to work"
-    end
+    @ask = params[:question]
+    @answer = 'Great!' if @ask == 'I am going to work'
+    @answer = 'Silly question,get dressed and go to work' if @ask.end_with?('?')
+    @answer = "I don't care, get dressed and go to work"
   end
 end
